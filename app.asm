3835b6839429d52c7ce51733e621ad005e18103e
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2022-02-16 16:55:03 +0100

    Create app.asm

---
+++ app.asm
@@ -0,0 +1,22 @@
+include \masm32\include\masm32rt.inc
+
+.code
+tetra:
+   mov      ebx, eax
+   inc      ebx
+   mul      ebx
+
+   inc      ebx
+   mul      ebx
+
+   mov      ebx, 6
+   div      ebx
+
+   print    str$(eax)
+
+   ret
+
+main:
+   mov eax, 2
+   call tetra
+END main
